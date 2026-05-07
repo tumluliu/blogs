8dcc3020e360917333b67374e85863ea4c618bc2
Lu Liu <nudtlliu@gmail.com>
2026-05-07 13:07:19 +0200

    chore(test): include src/**/*.test.ts in vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    include: ['scripts/**/*.test.ts'],
+    include: ['scripts/**/*.test.ts', 'src/**/*.test.ts'],
     environment: 'node',
   },
 });
